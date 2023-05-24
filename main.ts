de84cdd0a5b9401843bfe6e1e38bc51c8a0722af
Jake <72697402+FoodiePig@users.noreply.github.com>
2023-05-24 14:31:44 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onGesture(Gesture.Shake, function () {
+    num = randint(1, 6)
+})
+let num = 0
+num = 0
+basic.forever(function () {
+    if (num < 0) {
+        basic.showNumber(num)
+    } else {
+        basic.clearScreen()
+    }
+})
